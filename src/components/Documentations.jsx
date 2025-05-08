a114d2bec5bd2c66ede88dd8d3ec91a490d54bcb
Giridharan7193 <giridharan7193gs@gmail.com>
2025-05-08 13:14:48 +0530

    Docs Update

--- src/components/Documentations.jsx
+++ src/components/Documentations.jsx
@@ -1,7 +1,19 @@
+import { SearchIcon } from "lucide-react";
+
 export default function Documentation() {
     return (
         <>
-            Hello from docs
+            <div className="h-[400px] w-full relative">
+                <img src="https://wallpaperbat.com/img/8627514-computer-system-wallpaper-free.jpg" className="h-[400px] w-screen" />
+                <div className="absolute inset-0 flex flex-col items-center justify-center gap-[20px]">
+                    <p className="text-white text-[34px] font-semibold">Search Documentation</p>
+                    <div className="bg-white rounded-[10px] w-[850px] h-[60px] flex flex-row gap-[10px] items-center px-[20px] shadow-2xl">
+                        <SearchIcon className="text-gray-700 mr-[10px]" />
+                        <input placeholder="Type your queries..." className="outline-none w-[800px]" />
+                        <button className="cursor-pointer bg-black/80 hover:bg-black/90 rounded-[10px] text-white h-[40px] px-[10px]">Search</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
